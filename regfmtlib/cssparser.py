8764917e09db68d1c9f59930be31dc1b19d2bbd2
Charles Choi <kickingvegas@gmail.com>
2022-10-09 01:49:42 -0700

    Checkpoint commit

--- regfmtlib/cssparser.py
+++ regfmtlib/cssparser.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 import tinycss2
 from regfmtlib.cssstyles import *
 
@@ -27,6 +29,19 @@ def parseCSS(configFileName: str, styleSheet: StyleSheet):
                 declarations = tinycss2.parse_declaration_list(rule.content,
                                                                skip_comments=True,
                                                                skip_whitespace=True)
+
+                parseErrors = filter(lambda x: isinstance(x, tinycss2.ast.ParseError), declarations)
+                for parseError in parseErrors:
+                    message = 'ERROR: {}: line {}, col {}: {}\nExiting.\n'.format(configFileName,
+                                                                  parseError.source_line,
+                                                                  parseError.source_column,
+                                                                  parseError.message)
+
+
+                    sys.stderr.write(message)
+                    sys.exit(1)
+
+
                 for declaration in declarations:
 
                     try:
@@ -36,5 +51,7 @@ def parseCSS(configFileName: str, styleSheet: StyleSheet):
 
                         print('   {0}: {1} {2}'.format(declaration.name, declaration.value,
                                                        tinycss2.serialize(declaration.value).strip()))
+
                     except:
+                        # TODO: Need to handle all the exceptions.
                         print('hunh')
